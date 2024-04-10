c69cb3efa31d854f7595e9626abcc11c93e6a9ca
Sachin Gyandor <gyandors@gmail.com>
2024-04-10 15:18:06 +0530

    Added summary for restaurant

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -15,7 +15,18 @@ export default function Header() {
         <HeaderCartButton />
       </header>
       <div className={styles['meals-img']}>
-        <img src={MealsImage} alt="Meals backgroung" />
+        <img src={MealsImage} alt="Meals background" />
+        <div className={styles.summary}>
+          <h1>Delicious Food, Delivered To You</h1>
+          <p>
+            Choose your favorite meal from our broad selection of available
+            meals and enjoy a delicious lunch or dinner at home.
+          </p>
+          <p>
+            All our meals are cooked with high-quality ingredients, just-in-time
+            and of course by experienced chefs!
+          </p>
+        </div>
       </div>
     </>
   );
